95346bd5a1de9aa2d19140f96932eb268134802b
vadila234git <155548075+vadila234git@users.noreply.github.com>
2024-10-20 13:35:20 +0300

    added launch and models

--- nmt_progresion/autorization_app/models.py
+++ nmt_progresion/autorization_app/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
-# Create your models here.
+class User(models.Model):
+    name = models.CharField(max_length = 35)
+    surname = models.CharField(max_length = 35)
+    fatherly = models.CharField(max_length = 35, blank = True, default = False)
+    grade = models.CharField(max_length = 5, blank = True, default = False)
+    occupation = models.CharField(max_length = 35)
+    password = models.CharField(max_length = 35, default = False)
+    date_of_birth = models.DateField()
